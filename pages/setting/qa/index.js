ac66a331d712caa1b661fd1985759c7bed424d58
Uzemiu <1449851534@qq.com>
2023-03-21 11:46:18 +0800

    add gpt tooltip

--- pages/setting/qa/index.js
+++ pages/setting/qa/index.js
@@ -14,5 +14,13 @@ Page({
     this.setData({
       useGpt: detail
     })
+  },
+  showTooltip(){
+    wx.showModal({
+      title: '提示',
+      content: `该模式可能使用包括但不限于GPT在内的大规模语言模型生成回答，尽管我们已经尽力确保其准确性，但仍然可能存在错误或偏差。
+      我们会记录您发送的问题和生成的回答，这些数据将被用于后续的训练，以此来提高模型的性能和表现。`,
+      showCancel: false
+    })
   }
 })
